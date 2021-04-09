327885137b4a6a12d165356728e9932752a74427
bezhenareugen <eugenbezhenar@gmail.com>
2021-04-09 13:42:06 +0300

    Final Result

---
+++ src/GildedRose.Console/Bll/UpdateQuality.cs
@@ -0,0 +1,110 @@
+﻿using GildedRose.Console.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Console.Bll
+{
+    class UpdateQuality : IUpdateQuality
+    {
+        public void UpdateQualityMethod(List<Item> Items)
+        {
+            foreach (var item in Items)
+            {
+                switch (item.Name)
+                {
+                    case "Aged Brie":
+                        UpdateAgeBrieQuality(item);
+                        break;
+                    case "Sulfuras, Hand of Ragnaros":
+                        UpdateSulfarusQuality(item);
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        UpdateBackStageQuality(item);
+                        break;
+                    case "Conjured Mana Cake":
+                        UpdateConjuredItem(item);
+                        break;
+                    default:
+                        UpdateCommonItem(item);
+                        break;
+                }
+            }
+        }
+
+
+        public void UpdateAgeBrieQuality(Item item)
+        {
+            item.SellIn--;
+
+            if (item.Quality < 50)
+            {
+                item.Quality++;
+            }
+        }
+
+        public void UpdateBackStageQuality(Item item)
+        {
+            item.SellIn--;
+
+            if (item.Quality < 50)
+            {
+                if (item.SellIn < 0)
+                {
+                    item.Quality = 0;
+                }
+                else if (item.SellIn < 5)
+                {
+                    item.Quality += 3;
+                }
+                else if (item.SellIn < 10)
+                {
+                    item.Quality += 2;
+                }
+                else
+                {
+                    item.Quality++;
+                }
+            }
+        }
+
+        public void UpdateCommonItem(Item item)
+        {
+            item.SellIn--;
+
+            if (item.SellIn < 0)
+            {
+                item.Quality -= 2;
+            }
+            else
+            {
+                item.Quality--;
+            }
+        }
+
+        public void UpdateConjuredItem(Item item)
+        {
+            item.SellIn--;
+
+            if (item.SellIn < 0)
+            {
+                item.Quality -= 4;
+            }
+            else
+            {
+                item.Quality -= 2;
+            }
+        }
+
+        public void UpdateSulfarusQuality(Item item)
+        {
+            if (item.Quality < 80)
+            {
+                item.SellIn = item.SellIn;
+                item.Quality = item.Quality;
+            }
+        }
+    }
+}
